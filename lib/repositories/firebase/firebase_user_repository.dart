37367bf51a770e11e0de354c975a01a747cf5763
KenAra <louis.9814@gmail.com>
2020-10-18 17:03:25 +0900

    リポジトリ：重複解除により、二つ名を削除

--- lib/repositories/firebase/firebase_user_repository.dart
+++ lib/repositories/firebase/firebase_user_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tasuke_ai/model/user/user_repository.dart';
-import 'package:tasuke_ai/model/user/user.dart' as UserModel;
+import 'package:tasuke_ai/model/user/user.dart';
 
 class FirebaseUserRepository implements UserRepository {
   CollectionReference _query;
@@ -11,7 +11,7 @@ class FirebaseUserRepository implements UserRepository {
   }
 
   @override
-  Future store({@required UserModel.User user}) async {
+  Future store({@required User user}) async {
     await _query
       .doc(user.uid.value.toString())
       .set(user.toMap());
