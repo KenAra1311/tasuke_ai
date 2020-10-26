429edfd7486d62e4f954066f139a2e11c8629a0f
KenAra <louis.9814@gmail.com>
2020-10-26 16:32:30 +0900

    投稿画面：入力欄（基礎）の設置

--- lib/pages/home/post_help/post_help_page.dart
+++ lib/pages/home/post_help/post_help_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tasuke_ai/widgets/molecules/overlay_loading.dart';
 import 'package:tasuke_ai/widgets/organisms/header.dart';
 
 import 'post_help_page_view_model.dart';
@@ -26,8 +29,52 @@ class PostHelpPage extends StatelessWidget {
 class _Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(),
+    final _postHelpPageModelView = Provider.of<PostHelpPageModelView>(context);
+
+    return Stack(
+      fit: StackFit.expand,
+      overflow: Overflow.clip,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 30, bottom: 50),
+                  alignment: Alignment.topCenter,
+                  child: Text('家事や力を貸してほしいことなどを発信しましょう！'),
+                ),
+                TextFormField(
+                  controller: _postHelpPageModelView.titleTextController,
+                  decoration: InputDecoration(
+                    labelText: 'タイトル',
+                    errorText: _postHelpPageModelView.titleValidate ? 'タイトルは必ず入力してください' : null
+                  ),
+                ),
+                TextFormField(
+                  controller: _postHelpPageModelView.descriptionTextController,
+                  decoration: InputDecoration(labelText: '説明'),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  width: double.infinity,
+                  height: 40,
+                  child: RaisedButton(
+                    onPressed: () => null,
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    child: Text('投稿する'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        OverlayLoading(visible: _postHelpPageModelView.visible),
+      ],
     );
   }
 }
--- lib/pages/home/post_help/post_help_page_view_model.dart
+++ lib/pages/home/post_help/post_help_page_view_model.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/cupertino.dart';
 
-class PostHelpPageModelView extends ChangeNotifier {}
+class PostHelpPageModelView extends ChangeNotifier {
+  TextEditingController _titleTextController;
+  TextEditingController _descriptionTextController;
+  bool _titleValidate;
+  bool _visible;
+
+  PostHelpPageModelView() {
+    _titleTextController = TextEditingController();
+    _descriptionTextController = TextEditingController();
+    _titleValidate = false;
+    _visible = false;
+  }
+
+  TextEditingController get titleTextController => _titleTextController;
+  TextEditingController get descriptionTextController => _descriptionTextController;
+  bool get titleValidate => _titleValidate;
+  bool get visible => _visible;
+}
