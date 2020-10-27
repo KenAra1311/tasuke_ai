187b1e3cc571cf8cd64de965e8d6b661dc8b6959
KenAra <louis.9814@gmail.com>
2020-10-27 19:34:15 +0900

    投稿：日付を選択できるように改修

--- lib/pages/home/post_help/post_help_page.dart
+++ lib/pages/home/post_help/post_help_page.dart
@@ -60,8 +60,52 @@ class _Body extends StatelessWidget {
                   keyboardType: TextInputType.multiline,
                   maxLines: 3,
                 ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      child: Text('日付'),
+                    ),
+                    Container(
+                      width: 200,
+                      height: 40,
+                      child: RaisedButton(
+                        onPressed: () => _postHelpPageModelView.selectDate(context: context),
+                        color: Colors.white.withOpacity(0.7),
+                        textColor: Colors.redAccent,
+                        child: Text(_postHelpPageModelView.date),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      child: Text('時間'),
+                    ),
+                    Container(
+                      width: 200,
+                      height: 40,
+                      child: RaisedButton(
+                        onPressed: () => null,
+                        color: Colors.white.withOpacity(0.7),
+                        textColor: Colors.redAccent,
+                        child: Text(_postHelpPageModelView.time),
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  controller: _postHelpPageModelView.titleTextController,
+                  decoration: InputDecoration(
+                    labelText: 'おおよそかかる時間',
+                    hintText: 'およそ１時間程度',
+                    errorText: _postHelpPageModelView.titleValidate ? 'タイトルは必ず入力してください' : null
+                  ),
+                ),
                 Container(
-                  margin: EdgeInsets.only(top: 30),
+                  margin: EdgeInsets.only(top: 30, bottom: 30),
                   width: double.infinity,
                   height: 40,
                   child: RaisedButton(
--- lib/pages/home/post_help/post_help_page_view_model.dart
+++ lib/pages/home/post_help/post_help_page_view_model.dart
@@ -1,21 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:tasuke_ai/service/home/post_help/post_help_service.dart';
 import 'package:tasuke_ai/service/validator/post_validator.dart';
+import 'package:tasuke_ai/widgets/atoms/show_date_picker.dart';
 
 class PostHelpPageModelView extends ChangeNotifier {
   PostHelpService _postHelpService;
   PostValidator _postValidator;
   TextEditingController _titleTextController;
   TextEditingController _descriptionTextController;
+  String _date;
+  String _time;
   bool _titleValidate;
   bool _visible;
   String _info;
 
   PostHelpPageModelView() {
+    initializeDateFormatting('ja');
+
     _postHelpService = PostHelpService();
     _postValidator = PostValidator();
     _titleTextController = TextEditingController();
     _descriptionTextController = TextEditingController();
+    _date = DateFormat.yMMMd('ja').format(DateTime.now());
+    _time = DateFormat.Hm('ja').format(DateTime.now());
     _titleValidate = false;
     _visible = false;
     _info = '';
@@ -23,9 +32,23 @@ class PostHelpPageModelView extends ChangeNotifier {
 
   TextEditingController get titleTextController => _titleTextController;
   TextEditingController get descriptionTextController => _descriptionTextController;
+  String get date => _date;
+  String get time => _time;
   bool get titleValidate => _titleValidate;
   bool get visible => _visible;
   String get info => _info;
+
+  /*
+   * 日付の選択操作を実行させる
+   */
+  Future<void> selectDate({@required BuildContext context}) async {
+    _date = await ShowDatePicker(
+      context: context,
+      date: date,
+    ).selectDate();
+
+    notifyListeners();
+  }
 
   /*
    * 投稿を実行し、バリデーションがOKならば保存＆リダイレクトを行う
